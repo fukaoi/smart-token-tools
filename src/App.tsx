4e7acb9dd09748d82f992cd6251aa1e018112df5
fukaoi <goodakira7@gmail.com>
2022-01-08 20:26:02 +0900

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import {useState} from 'react';
-import './App.css';
-import {
-  Button,
-} from '@mui/material';
+import {Button} from '@mui/material';
+import {makeStyles} from '@mui/styles';
 
 import Header from './components/Header';
 import Token from './pages/Token';
 import WellComeMessage from './components/WellComeMessage';
 
+import backImage from './assets/background-image.jpg';
+
+const useStyles = makeStyles({
+  app: {
+    textAlign: 'center',
+    margin: 0,
+    fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 
+    'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif`,
+    '-webkit-font-smoothing': 'antialiased',
+    '-moz-osx-font-smoothing': 'grayscale',
+  },
+  appMain: {
+    backgroundSize: 'cover',
+    backgroundImage: `url(${backImage})`,
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'ceter',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white',
+  }
+});
 
 const App = () => {
+  const styles = useStyles();
   const [content, setContent] = useState('Main');
 
   const callWalletAdapter = () => {
@@ -17,13 +40,12 @@ const App = () => {
   }
 
   return (
-    <div className='App'>
+    <div className={styles.app}>
       <Header />
-      <main className='App-main'>
+      <main className={styles.appMain}>
         {content == 'Token'
           ? <Token />
-          :
-          <div>
+          : <div>
             <WellComeMessage />
             <Button
               variant='contained'
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import {AppBar, Toolbar, Grid} from '@mui/material';
 import Button from '@mui/material/Button';
+import headerLogo from '../assets/atonoy-logo.png';
 
 const pages = ['Token', 'NFT', 'Contact'];
 const Header = () => {
   return (
-    <AppBar position="static" color="transparent" >
+    <AppBar position='static' color='transparent'>
       <Toolbar>
         <Grid container>
           <Grid item xs={2}>
-            <div className="Header-logo" />
+            <img src={headerLogo} />
           </Grid>
           <Grid item xs={7}>
           </Grid>
