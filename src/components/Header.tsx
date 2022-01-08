5da2258c505250a775102073c853fcd4be4b0609
fukaoi <goodakira7@gmail.com>
2022-01-08 17:19:13 +0900

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,30 @@
-import {AppBar, Toolbar} from '@mui/material';
+import {AppBar, Toolbar, Grid} from '@mui/material';
 import Button from '@mui/material/Button';
 
-const pages = ['Token', 'NFT', 'CONTACT'];
+const pages = ['Token', 'NFT', 'Contact'];
 
 const Header = () => {
   return (
     <AppBar position="static" color="transparent" >
-      <Toolbar>
-        <div className="Header-logo" />
-        {pages.map((page) => (
-          <Button
-            key={page}
-            onClick={console.log}
-            sx={{my: 2, color: 'black', display: 'block' }}
-          >
-            {page}
-          </Button>
-        ))}
+      <Toolbar sx={{height: 70}}>
+        <Grid container>
+          <Grid item xs={1}>
+            <div className="Header-logo" />
+          </Grid>
+          <Grid item xs={8}>
+          </Grid>
+          <Grid item xs={3}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={console.log}
+                sx={{my: 1, color: 'black'}}
+              >
+                {page}
+              </Button>
+            ))}
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
