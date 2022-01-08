6f310d67c6708e62615a667f408db35eb4a9aa07
fukaoi <goodakira7@gmail.com>
2022-01-08 21:53:03 +0900

    updatred

--- src/components/WellComeMessage.tsx
+++ src/components/WellComeMessage.tsx
@@ -1,8 +1,27 @@
+import {makeStyles} from '@mui/styles';
+
+import titleLogo from '../assets/title-logo.png';
+
+const useStyles = makeStyles({
+  message: {
+    marginBottom: '15em',
+  },
+  wellCome: {
+    fontSize: '1.8em',
+    fontWeight: 'bolder',
+    marginBottom: '0.2em'
+  },
+  title: {
+    fontSize: '3em',
+  }
+});
+
 const WellComeMessage = () => {
+  const styles = useStyles();
   return (
-    <div>
-      <div>Well Come</div>
-      <div>SMART TOKEN TOOL</div>
+    <div className={styles.message}>
+      <div className={styles.wellCome}>Well Come</div>
+      <img src={titleLogo} />
     </div>
   );
 }
