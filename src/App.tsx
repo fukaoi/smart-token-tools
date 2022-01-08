e13d4a4a47c2a791e25dad8645298d3183086c43
fukaoi <goodakira7@gmail.com>
2022-01-08 12:52:50 +0900

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
 import './App.css';
 import {
   Button,
-  createTheme,
-  ThemeProvider,
 } from '@mui/material';
 
 import Header from './components/Header';
 
 const App = () => {
-  const theme = createTheme({
-    palette: {
-      mode: 'light',
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
+    <div className="App">
       <Header />
-      <div className="App">
-        <header className="App-header">
-          <Button variant="contained" color="secondary">Wallet Connect</Button>
-        </header>
-      </div>
-    </ThemeProvider>
+      <main className="App-main">
+        <Button
+          variant="contained"
+          size="large"
+          color='secondary'
+        >Wallet Connect</Button>
+      </main>
+    </div>
   );
 }
 
