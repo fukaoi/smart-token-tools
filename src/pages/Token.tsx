69857a44d1549e519c9119b5edf665dda6a64cfa
fukaoi <goodakira7@gmail.com>
2022-01-18 11:22:08 +0900

    Wip: fix transfer

--- src/pages/Token.tsx
+++ src/pages/Token.tsx
@@ -1,6 +1,52 @@
-import React from 'react';
+import {useEffect} from 'react';
+// import * as core from '@solana-suite/core';
+// import * as shared from '@solana-suite/shared';
+import {
+  TransactionInstruction,
+  Transaction,
+  PublicKey,
+  Connection,
+  SystemProgram
+} from '@solana/web3.js';
+
+export const encode = (data: string): Buffer => Buffer.from(data);
+
+const userPublicKey = new PublicKey('Gd5ThBjFzEbjfbJFGqwmBjDXR9grpAdqzb2L51viTqYV');
+const receiptPublicKey = new PublicKey('9vguAhFraAyv8QGukf8MvsLE3HHWKN3LqHMKMKtSirv3');
+
+// const instruction = new TransactionInstruction({
+// programId: usrePublicKey,
+// data: encode('data'),
+// keys: [{
+// pubkey: new PublicKey('Gd5ThBjFzEbjfbJFGqwmBjDXR9grpAdqzb2L51viTqYV'),
+// isSigner: true,
+// isWritable: true
+// }]
+// });
+
+const instruction = SystemProgram.transfer({
+  fromPubkey: userPublicKey,
+  toPubkey: receiptPublicKey,
+  lamports: 10 //Investing 1 SOL. Remember 1 Lamport = 10^-9 SOL.
+});
 
 const Token = () => {
+  useEffect(() => {
+    window.solana.connect().then(async (wallet: any) => {
+      console.log(wallet.publicKey.toString(), wallet);
+      const connection = new Connection('http://api.devnet.solana.com');
+      const transaction = new Transaction();
+      let blockhashObj = await connection.getRecentBlockhash();
+      transaction.recentBlockhash = blockhashObj.blockhash;
+      const tx = transaction.add(instruction);
+      tx.feePayer = userPublicKey;
+      const signed = await window.solana.signTransaction(tx);
+      console.log(signed);
+      const sig = await connection.sendRawTransaction(signed.serialize());
+      console.log(sig);
+    });
+  }, []);
+
   return (
     <div>
       Token page
