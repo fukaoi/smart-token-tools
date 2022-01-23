645f1b4ace544bd5492aff9661f1d0c6e9bbce52
fukaoi <goodakira7@gmail.com>
2022-01-23 10:26:08 +0900

    updated

--- src/pages/Token.tsx
+++ src/pages/Token.tsx
@@ -2,18 +2,21 @@ import assert from 'assert';
 import {useEffect} from 'react';
 import {SplToken} from '../solanaSuites/spl-token';
 
-export const encode = (data: string): Buffer => Buffer.from(data);
+const mint = () => {
+  window.solana.connect().then(async (wallet: any) => {
+    const sig = await SplToken.createMint(
+      wallet.publicKey,
+      1,
+      window.solana.signTransaction
+    );
+    sig.isErr && assert(sig);
+  });
+}
+
 
 const Token = () => {
   useEffect(() => {
-    window.solana.connect().then(async (wallet: any) => {
-      const sig = await SplToken.createMint(
-        wallet.publicKey,
-        1,
-        window.solana.signTransaction
-      );
-      sig.isErr && assert(sig);
-    });
+    mint();
   }, []);
 
   return (
