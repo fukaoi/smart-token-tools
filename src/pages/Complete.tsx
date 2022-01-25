14f38920fc9c3a35ae62c2b77d9b032653a33e34
fukaoi <goodakira7@gmail.com>
2022-01-25 22:33:41 +0900

    updated

--- src/pages/Complete.tsx
+++ src/pages/Complete.tsx
@@ -1,7 +1,10 @@
+import PageTitle from "../components/PageTitle";
+
 const Complete = () => {
   return (
     <div>
-      <h1>Congratulations!</h1>
+      <PageTitle title='Complete' />
+      <h5>Congratulations!</h5>
     </div>
   );
 };
