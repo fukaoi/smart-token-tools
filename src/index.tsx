970df2bdcf3d1b35a4c60d1c2a51ec5bed3f9cd3
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-08-08 15:38:45 +0900

    react18 upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import {theme} from './shared/colorTheme';
-import {ThemeProvider} from '@mui/material/styles';
-import {HashRouter as Router} from 'react-router-dom';
-import {Buffer as buffer} from 'buffer';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { theme } from "./shared/colorTheme";
+import { ThemeProvider } from "@mui/material/styles";
+import { HashRouter as Router } from "react-router-dom";
+import { Buffer as buffer } from "buffer";
 
 global.Buffer = buffer; // @solana/spl-token Fix for raise error
- 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme} >
-      <Router>
-        <App />
-      </Router>
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+if (container) {
+  root.render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
 reportWebVitals();
