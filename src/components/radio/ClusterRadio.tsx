2dc91ea3688a7d89e2d0ba9e9c7950991a5163c6
fukaoi <usapyon2022@gmail.com>
2022-12-07 11:03:00 +0900

    Changed cluster value by Constants module

--- src/components/radio/ClusterRadio.tsx
+++ src/components/radio/ClusterRadio.tsx
@@ -3,25 +3,30 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import HeadlineTypography from '../typography/HeadlineTypography';
 import { useController, UseControllerProps } from 'react-hook-form';
+import { Constants } from '@solana-suite/shared';
 
 const ClusterRadio = (props: UseControllerProps<any>) => {
   const { field } = useController(props);
   return (
     <>
       <HeadlineTypography message="Select your using network cluster" />
-      <RadioGroup aria-labelledby="cluster" defaultValue="devnet" {...field}>
+      <RadioGroup
+        aria-labelledby="cluster"
+        defaultValue={Constants.Cluster.dev}
+        {...field}
+      >
         <FormControlLabel
-          value="mainnet-beta"
+          value={Constants.Cluster.prdMetaplex}
           control={<Radio color="secondary" />}
           label="Mainnet-beta"
         />
         <FormControlLabel
-          value="devnet"
+          value={Constants.Cluster.dev}
           control={<Radio color="warning" />}
           label="Devnet"
         />
         <FormControlLabel
-          value="testnet"
+          value={Constants.Cluster.test}
           control={<Radio color="primary" />}
           label="Testnet"
         />
