efd43ef1c73313cbb2a9a5e6e3aeede099154cbd
fukaoi <usapyon2022@gmail.com>
2024-06-17 19:35:32 +0900

    updated

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,11 @@ import {
 } from "@remix-run/react";
 import "./tailwind.css";
 
+import backImage from "./assets/background-image.jpg";
+import logoImage from "./assets/smt-logo.svg";
+import { Box, Grid, useMediaQuery } from "@mui/material";
+import { Device } from "./utils//device";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
