860b5037a03199da658cccab050fa1d50c1e690a
fukaoi <usapyon2022@gmail.com>
2024-08-20 10:13:46 +0900

    updated

--- app/components/provider/AppWalletProvider.tsx
+++ app/components/provider/AppWalletProvider.tsx
@@ -8,25 +8,15 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { useStorage } from "~/utils/storage";
-import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
 
-const AppWalletProvider = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
+const AppWalletProvider = ({ children }: { children: React.ReactNode }) => {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   const [storage, setStorage] = useStorage("network");
   if (!storage) {
     setStorage({ cluster: network });
   }
-  const wallets = useMemo(
-    () => [
-      new UnsafeBurnerWalletAdapter(),
-    ],
-    [network],
-  );
+  const wallets = useMemo(() => [], [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
